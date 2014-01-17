be63f66d482f811a548cf3ca8977ce64ed9c768a
SiepeAssassina <lorenzo.tomasin@live.it>
2014-01-17 20:45:32 +0100

    Fixed display code, needs testing.

--- voltdips/voltdips.ino
+++ voltdips/voltdips.ino
@@ -1,10 +1,9 @@
 #include <cstdlib>
 #include <math.h>
-#include <LiquidCrystal.h>
 #define _RESOLUTION 1000
 /*----------------------------------------------------------------\
 // Display code works as follows:                                 |
-//                                                                |
+//A3 Volt units -> A0 mills                                       |
 //                                                                |
 //              2                                                 |
 //           #######                                              |
@@ -21,7 +20,6 @@ int tensione; //valore dela tensione
 
 int SEG[8]={2, 3, 4, 5, 6, 7, 8, 9}; //array colonne
 int Vp[4]={A3, A2, A1, A0}; //Tensione per ogni quadrante
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 boolean NMatrix[10][8] = {
   {1, 1, 1, 1, 1, 1, 0, 0},  //0
   {0, 1, 1, 0, 0, 0, 0, 0},  //1
@@ -36,23 +34,22 @@ boolean NMatrix[10][8] = {
   
 void setup() 
 {
-  lcd.begin(16, 2);
   pinMode (9,OUTPUT); //puntino
   for(int i = 0; i <= 7; i++)
   {
-    pinMode (SEG[i], OUTPUT);                //abilito pin segmenti
-    digitalWrite (SEG[i],HIGH);              //livello iniziale segmenti 1
+    pinMode (SEG[i], OUTPUT);                
+    digitalWrite (SEG[i],HIGH);             
   }
   for(int i = 0; i <= 4; i++)
   {
-    pinMode (Vp[i], OUTPUT);                 //abilito pin alimentazioni
-    digitalWrite (Vp[i], LOW);               //livello iniziale 0
+    pinMode (Vp[i], OUTPUT);               
+    digitalWrite (Vp[i], LOW);             
   }
 }
 
 void loop() 
 {
-  for(int i = 0; i < _RESOLUTION; i++)                 //ciclo da 20
+  for(int i = 0; i < _RESOLUTION; i++)                 
   {      
     campioni += (float)(analogRead(A5) * (float)(5.0 / 1.023));  
   }                                      
@@ -61,31 +58,25 @@ void loop()
   update();                              
 }
 
-void update()
-{
-  delay(10);
-  lcd.clear();
-  lcd.print(tensione / _RESOLUTION);  
-  lcd.write('.');
-  lcd.print(tensione % _RESOLUTION);
-}
-
-/*void update()  
+void update()  
 {  
-  byte voltageFigures[3] = {tensione / 100, };   
+  byte voltageFigures[4] = {tensione / 1000, (tensione % 1000) / 100, (tensione % 100) / 10, tensione % 10};
   for(int i = 0; i < 4; i++)
-  {
-    digitalWrite (Vp[i], HIGH);     
+  {         
     for(int j = 0; j < 8; j++)
     {
       if(NMatrix[voltageFigures[i]][j])      
         digitalWrite(SEG[j], LOW);           
       else
         digitalWrite(SEG[j], HIGH);
-        delay(1);
-        digitalWrite (Vp[i], LOW);
+      if(i == 4)
+      {
+        digitalWrite(SEG[7], LOW);
+      }  
+      digitalWrite (Vp[i], HIGH);      
+      delay(2);
+      digitalWrite (Vp[i], LOW);
     }     
   }
 }
 
-*/
